0204873993868bac6f34220d1996bfa5bf629df1
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-11-16 13:05:32 -0300

    changes related to easier mod development

--- assets/scenes/ending.lua
+++ assets/scenes/ending.lua
@@ -1,5 +1,6 @@
-return function() local self = {}
-    self.dialogue = require "objects.dialogue" (nil, "fnt_default_big", 52, 272)
+return function(endingtext,intro) local self = {}
+    self.dialogue = require "objects.dialogue" (nil, "fnt_default_big", 52, 272, "SND_TXT1.wav")
+    self.dialoguebox = require "objects.dialoguebox" (32, 250, 576, 140)
     self.queue = require "objects.queue" ()
     self.dialoguetext = {}
     function self:nextdialogue()
@@ -8,6 +9,11 @@ return function() local self = {}
             local func = text[2]
             text = text[1]
             if func then func() end
+        end
+        if text == "" or text == nil then
+            self.dialoguebox.hidden = true
+        else
+            self.dialoguebox.hidden = false
         end
         if text then self.dialogue:settext(text) end
     end
@@ -31,30 +37,43 @@ return function() local self = {}
         self:onupdate(dt)
     end
     function self:draw()
+        self.dialoguebox:draw()
         self.dialogue:draw()
     end
-    local dialogue = {"* (Ring Ring...)","* Hello? is anyone there?", "* Welp, i guess i'll leave a message"}
-    if love.math.random(1,100) == 1 then
-        table.insert(dialogue,{"* Thanks for playing my little Undertale fan game!",function() MUSIC("z_ending.ogg"):play() MUSIC("z_ending.ogg"):setLooping(true) end})
-        table.insert(dialogue,"* Made with Love2D and a lot of coffee :D")
-        table.insert(dialogue,"* See you next time!")
-        table.insert(dialogue,"(No coffe was harmed on the production of this software\n  i hate coffe)")
-    else
-        table.insert(dialogue, {"* This is an ending scene test text",function() MUSIC("z_ending.ogg"):play() MUSIC("z_ending.ogg"):setLooping(true) end})
-        table.insert(dialogue, "* You can use this to make other\n  endings")
-        table.insert(dialogue, "* ererer er")
-        table.insert(dialogue, "* look at this shiny code, its soo\n  cool wow")
-        table.insert(dialogue, "* ererer er, ererer er er er")
-        table.insert(dialogue, "* this might be innacurate...")
-        table.insert(dialogue, "* where is the dialogue at :(?")
-        table.insert(dialogue, "* ending text")
-        table.insert(dialogue, "")
-        table.insert(dialogue, "* You though that was the last text?\n  no lol")
-        table.insert(dialogue, "* The underground has got a lot\n  quieter since you came here...")
-        table.insert(dialogue, "* what did you do?")
-        table.insert(dialogue, "* i can't go to hell, im out of\n  vacation days ;)")
-        table.insert(dialogue, "* real ending text")
-        table.insert(dialogue, "")
+    function self:addDialogue(dialogue, text, potrait)
+        local dialoguetext = {text, function() self.dialogue:setpotrait(potrait) end}
+        table.insert(dialogue,dialoguetext)
+    end
+    local dialogue = endingtext or {}
+    if not intro or intro == true then
+        table.insert(dialogue,"* (Ring Ring...)")
+        table.insert(dialogue,{"* Hello? is anyone there?",function() self.dialogue:setspeaker("sans") end})
+        self:addDialogue(dialogue,"* Welp, i guess i'll leave a\n  message","chuckle")
+        table.insert(dialogue,{"",function() MUSIC("z_ending.ogg"):play() MUSIC("z_ending.ogg"):setLooping(true) end})
+    end
+    if not endingtext then
+        if love.math.random(1,100) == 1 then
+            table.insert(dialogue, "* Thanks for playing my little Undertale fan game!")
+            table.insert(dialogue, "* Made with Love2D and a lot of coffee :D")
+            table.insert(dialogue, "* See you next time!")
+            table.insert(dialogue, "(No coffe was harmed on the production of this software\n  i hate coffe)")
+        else
+            table.insert(dialogue, {"* This is an ending scene\n  test text",function() self.dialogue:setpotrait("wink") end})
+            table.insert(dialogue, {"* You can use this to make\n  other endings",function() self.dialogue:setpotrait("chuckle") end})
+            table.insert(dialogue, {"* ererer er",function() self.dialogue:setpotrait("chuckle2") end})
+            table.insert(dialogue, {"* look at this shiny code, \n  its soo cool wow",function() self.dialogue:setpotrait("normal") end})
+            table.insert(dialogue, {"* ererer er, ererer er er er",function() self.dialogue:setpotrait("blink") end})
+            self:addDialogue(dialogue, "* this might be innacurate...","chuckle")
+            self:addDialogue(dialogue, "* where is the dialogue at\n  :(?","blink")
+            self:addDialogue(dialogue, "* ending text","normal")
+            table.insert(dialogue, "")
+            self:addDialogue(dialogue, "* You though that was the\n  last text? no lol","chuckle2")
+            self:addDialogue(dialogue, "* The underground has got a\n  lot quieter since you came\n  here...","blink")
+            self:addDialogue(dialogue, "* what did you do?","noeyes")
+            self:addDialogue(dialogue, "* i can't go to hell, im out\n  of vacation days ;)","chuckle")
+            self:addDialogue(dialogue, "* real ending text","normal")
+            table.insert(dialogue, "")
+        end
     end
     self:endturn(dialogue)
 return self end
