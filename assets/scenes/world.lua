e531487242dfd0bd8fc5586f5da87485229bfb4e
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-11-16 13:02:24 -0300

    added a debug check to leave the scene and testing running

--- assets/scenes/world.lua
+++ assets/scenes/world.lua
@@ -1,10 +1,13 @@
 return function() local self = {}
     self.character = require "objects.character"
     local char = self.character()
+    char.canrun = true
     self.characters = {char}
     function self:update(dt)
         if ISPRESSED "CANCEL" then
-            RELOAD()
+            if DEBUG then
+                RELOAD()
+            end
         end
         for i,char in ipairs(self.characters) do
             char:update(dt)
